f5beaf42f2e072e4b46f0da85f2c370a8904f11b
Ian <yt2015.97@gmail.com>
2017-12-03 14:02:31 +0800

    修改字体与图片 loader为url-loader。limit=8192

--- webpack.config.js
+++ webpack.config.js
@@ -35,11 +35,21 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
-                use: ['file-loader']
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                      limit: 8192
+                    }
+                }
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: ['file-loader']
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                      limit: 8192
+                    }
+                }
             }
         ]
     },
